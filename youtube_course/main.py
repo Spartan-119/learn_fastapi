4423b81aba9a4d21c4266934bd59207720feafc2
Abin (אבין ברגיס) <abinvarghese90@gmail.com>
2025-01-30 06:24:07 +0000

    added youtube course practice content

--- youtube_course/main.py
+++ youtube_course/main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -11,15 +12,22 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [
+    {"title": "some title lol", "content": "some content", "id": 1},
+    {"title": "second title lol", "content": "second content lol", "id": 2}
+]
+
 @app.get("/")
 def root():
     return {"message": "hello world"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
 @app.post("/createposts")
 def create_posts(new_post: Post):
-    print(new_post.rating)
-    return {"data": "new post"}
+    post_dict = new_post.model_dump()
+    post_dict['id'] = randrange(0, 10000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
